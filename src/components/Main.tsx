3ee95440ee81aa253dc4728bd1ee333ee803fcbd
Viper <pruncle21@gmail.com>
2026-06-01 20:29:32 -0500

    Added content to Main.tsx to showcase core competencies. Holding off on implementing SVG icons or background-images to the container that is rendered via a .map() callback function.

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,22 @@
 export default function Main() {
+    const skillSection = ["React", "State Management", "TypeScript", "CSS Architecture", "Tailwind CSS", "Responsive Design"]
+
     return (
-        <div>
+        <section className="flex flex-col gap-20 w-full">
+            <div className="flex flex-col gap-10">
+                <h1 className="text-primary text-2xl font-semibold">Core Competencies</h1>
+                <div className="skills-container grid grid-rows-3 grid-cols-2 gap-5">
+                    {
+                        skillSection.map((item, index) => (
+                            <div className="flex flex-col justify-center items-center text-gray-400 border border-gray-600 rounded-md h-[30vw] text-center p-2" key={index}>
+                                {item}
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
             
-        </div>
+            <hr className="border-solid border-gray-900" />
+        </section>
     )
 }
